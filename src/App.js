c13a99dbb69dfc7cd3d87590dfdd2c50709fb7ac
Umer Hassan <umerhassan@Umers-MacBook-Pro.local>
2022-08-26 00:19:37 -0400

    improvement using the Transaction component and minor house keeping items

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from "react";
+import Transactions from "./components/Transactions/Transactions";
 
 function App() {
+
+  const [transactions, setTransactions] = useState([]);
+
+  useEffect(()=> {
+    fetch("http://localhost:3000/data/data.json")
+      .then(res => res.json())
+      .then((data) => setTransactions(data.transactions));
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Transactions transactions={transactions}/>
     </div>
   );
 }
